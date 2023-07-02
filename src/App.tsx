0d2ce6eaec447b31ed18f0db5991495d711a7952
Ian Davis <idavis@ball.com>
2023-07-02 19:58:02 -0400

    Working player movement implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import type { Component } from 'solid-js';
 
 import styles from './App.module.css';
-import { Board } from './board/board';
+import { Board } from './board/components/board';
 import { CodeIDE } from './ide/ide';
 
 const App: Component = () => {
+    const handleKeyPressed = (event: KeyboardEvent) => {
+        console.debug(event.key);
+    };
+
     return (
         <div class={styles.app_container}>
             <CodeIDE />
--- src/board/components/board.tsx
+++ src/board/components/board.tsx
@@ -1,3 +1,5 @@
+import { createSignal } from 'solid-js';
+import { Player } from '../../player/models/player.model';
 import { BackgroundTile } from '../tiles/components/background_tile';
 import styles from './board.module.css';
 
@@ -12,6 +14,43 @@ export const Board = () => {
     const numberColumns = Math.floor(width / tileWidth);
 
     const tiles = [];
+
+    const [getPlayer, setPlayer] = createSignal<Player>({
+        x: 0,
+        y: 0,
+    });
+
+    const handleKeyPressed = (event: KeyboardEvent) => {
+        if (event.key == 'w') {
+            setPlayer((player) => {
+                return {
+                    x: player.x,
+                    y: player.y + tileHeight,
+                };
+            });
+        } else if (event.key == 'd') {
+            setPlayer((player) => {
+                return {
+                    x: player.x + tileWidth,
+                    y: player.y,
+                };
+            });
+        } else if (event.key == 'a') {
+            setPlayer((player) => {
+                return {
+                    x: player.x - tileWidth,
+                    y: player.y,
+                };
+            });
+        } else if (event.key == 's') {
+            setPlayer((player) => {
+                return {
+                    x: player.x,
+                    y: player.y - tileHeight,
+                };
+            });
+        }
+    };
 
     for (let row = 0; row < numberRows; row++) {
         const y = row * tileHeight;
@@ -42,6 +81,8 @@ export const Board = () => {
             width={width}
             height={height}
             stroke={'black'}
+            tabIndex={'0'}
+            onKeyPress={handleKeyPressed}
         >
             <g>
                 <rect
@@ -50,6 +91,13 @@ export const Board = () => {
                     fill={'white'}
                 />
                 {tiles}
+                <rect
+                    width={tileWidth}
+                    height={tileHeight}
+                    fill={'yellow'}
+                    x={getPlayer().x}
+                    y={getPlayer().y}
+                />
             </g>
         </svg>
     );
