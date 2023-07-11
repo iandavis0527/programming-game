74493deb2ad3ddef66f0b0fde89c0b8ae2df36f0
Ian Davis <idavis@ball.com>
2023-07-11 15:53:28 -0400

    Adding type lib folder

---
+++ backend/client_api/type_lib/movement_commands.d.ts
@@ -0,0 +1,20 @@
+/**
+ * Move the character one tile north.
+ * Does NOT check to make sure a tile exists.
+ */
+declare function move_north(): void;
+/**
+ * Move the character one tile south.
+ * Does NOT check to make sure a tile exists.
+ */
+declare function move_south(): void;
+/**
+ * Move the character one tile west.
+ * Does NOT check to make sure a tile exists.
+ */
+declare function move_west(): void;
+/**
+ * Move the character one tile east.
+ * Does NOT check to make sure a tile exists.
+ */
+declare function move_east(): void;
